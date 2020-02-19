0831a85df82b2b570871fdf453e7c0e65b9a7b73
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-19 07:08:15 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  spots = Array.new(9, "   ")
+  spots = Array.new(9, " ")
   dashed_line = "-----------"
   
   puts "#{spots[0]} | #{spots[1]} | #{spots[2]}"
